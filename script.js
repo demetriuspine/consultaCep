8bdb7fe50ef40bd09f0e92972f41a9620232d5e4
lucas <mr.pine97@gmail.com>
2021-08-26 12:06:28 -0300

    movendo as funçoes para o window.onload

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const cep = document.querySelector('#cep');
-
-const insertValues = (resultsFromApi) => {
-  const resultsFromApiKeys = Object.keys(resultsFromApi);
-  for (let index = 0; index < resultsFromApiKeys.length; index += 1) {
-    if (document.querySelector(`#${resultsFromApiKeys[index]}`)) {
-      document.querySelector(`#${resultsFromApiKeys[index]}`)
-        .value = resultsFromApi[resultsFromApiKeys[index]];
-    }
-  }
-};
-
-cep.addEventListener('blur', async () => {
-  const search = cep.value.replace('-', '');
-  const options = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default',
-  };
-  try {
-    const fetching = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
-    const jsonParsing = await fetching.json();
-    insertValues(jsonParsing);
-  } catch (err) {
-    console.log(`'Deu erro:  ${err}`);
-  }
-});
-
 window.onload = () => {
+  const cep = document.querySelector('#cep');
 
+  const insertValues = (resultsFromApi) => {
+    const resultsFromApiKeys = Object.keys(resultsFromApi);
+    for (let index = 0; index < resultsFromApiKeys.length; index += 1) {
+      if (document.querySelector(`#${resultsFromApiKeys[index]}`)) {
+        document.querySelector(`#${resultsFromApiKeys[index]}`)
+          .value = resultsFromApi[resultsFromApiKeys[index]];
+      }
+    }
+  };
+
+  cep.addEventListener('blur', async () => {
+    const search = cep.value.replace('-', '');
+    const options = {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'default',
+    };
+    try {
+      const fetching = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
+      const jsonParsing = await fetching.json();
+      insertValues(jsonParsing);
+    } catch (err) {
+      console.log(`'Deu erro:  ${err}`);
+    }
+  });
 };
