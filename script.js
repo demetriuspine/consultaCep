49b91cabefb5455be356da9b42b9a7a391f6dcac
lucas <mr.pine97@gmail.com>
2021-08-26 11:59:04 -0300

    impl: addEventListener de blur + fecth async/await

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const cep = document.querySelector('#cep');
+
+cep.addEventListener('blur', async () => {
+  const search = cep.value.replace('-', '');
+  const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default',
+  };
+  try {
+    const fetching = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
+    const jsonParsing = await fetching.json();
+  } catch (err) {
+    console.log(`'Deu erro:  ${err}`);
+  }
+});
+
 window.onload = () => {
 
 };
