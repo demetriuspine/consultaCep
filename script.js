959195bb71776932766a9aa67e1045d104cc9888
lucas <mr.pine97@gmail.com>
2021-08-26 12:03:49 -0300

    impl: completeValues

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const cep = document.querySelector('#cep');
 
+const completeValues = (resultsFromApi) => {
+  const resultsFromApiKeys = Object.keys(resultsFromApi);
+  for (let index = 0; index < resultsFromApiKeys.length; index += 1) {
+    if (document.querySelector(`#${resultsFromApiKeys[index]}`)) {
+      document.querySelector(`#${resultsFromApiKeys[index]}`)
+        .value = resultsFromApi[resultsFromApiKeys[index]];
+    }
+  }
+};
+
 cep.addEventListener('blur', async () => {
   const search = cep.value.replace('-', '');
   const options = {
@@ -10,6 +20,7 @@ cep.addEventListener('blur', async () => {
   try {
     const fetching = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
     const jsonParsing = await fetching.json();
+    completeValues(jsonParsing);
   } catch (err) {
     console.log(`'Deu erro:  ${err}`);
   }
